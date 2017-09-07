ec6fbe642c6c3997875244edbd195682da94fd6d
kapilkumar <sk.kapilk@gmail.com>
2017-09-08 01:51:22 +0530

    Initial Commit

---
+++ JellyRole/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JellyRole
+//
+//  Created by Kapil Kumar on 9/8/17.
+//  Copyright © 2017 Kapil Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
